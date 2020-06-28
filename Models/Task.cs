8c326a0c1006cc9955d66f38d1e31fde3dc98036
Rafael Augusto de Oliveira <rafaop.19@gmail.com>
2020-06-28 16:09:46 +0000

    refac(models): Change task from date to string

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,4 +1,3 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -11,9 +10,9 @@ namespace Api.Models
         public int Id { get; set; }
 
         [Required]
-        public DateTime StartedAt { get; set; }
+        public string StartedAt { get; set; }
 
-        public DateTime EndedAt { get; set; }
+        public string EndedAt { get; set; }
 
         [Required]
         public string Status { get; set; }
